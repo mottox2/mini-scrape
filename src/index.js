64f129f5f7ed32600287ebc511767706ab0a1b70
mottox2 <y.mottox2@gmail.com>
2018-04-14 00:44:01 +0900

    Add src

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const request = require('request')
+const { JSDOM } = require('jsdom')
+
+const fetchBody = (url) => {
+  return new Promise((resolve, reject) => {
+    request(url, (e, response, body) => {
+      if (!e && response.statusCode === 200) {
+        resolve(body)
+      } else {
+        reject(error)
+      }
+    })
+  })
+}
+
+const scrape = (async (url, callback) => {
+  try {
+    const body = await fetchBody(url)
+    const dom = new JSDOM(body)
+    callback(dom.window)
+  } catch(e) {
+    console.error(e)
+  }
+})
+
+module.exports = scrape
