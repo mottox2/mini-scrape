3c453871521ebe1edd641027b2c6eb5eea673213
mottox2 <y.mottox2@gmail.com>
2018-04-16 23:13:35 +0900

    Bump 1.0.1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import request from 'request'
-import { JSDOM } from 'jsdom';
+import { JSDOM } from 'jsdom'
 
 const fetchBody = (url) => {
   return new Promise((resolve, reject) => {
-    request(url, (e, response, body) => {
-      if (!e && response.statusCode === 200) {
+    request(url, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
         resolve(body)
       } else {
         reject(error)
@@ -18,8 +18,8 @@ const scrape = (async (url, callback) => {
     const body = await fetchBody(url)
     const dom = new JSDOM(body)
     callback(dom.window)
-  } catch(e) {
-    console.error(e)
+  } catch(error) {
+    console.error(error)
   }
 })
 
